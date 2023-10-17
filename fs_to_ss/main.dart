4ed26205cfe7c3be38615535aee71b157de1a9c7
Ivan Gaydarzhi <112941873+vinny290@users.noreply.github.com>
2023-10-18 00:13:01 +0300

    Create main.dart

---
+++ fs_to_ss/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:music/first_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Returned Pass Data',
+    home: First_screen(),
+  ));
+}
