f989a95566f0d47d77e3179f9acbf3f2f5aac56e
Ivan Gaydarzhi <112941873+vinny290@users.noreply.github.com>
2023-10-19 01:14:14 +0300

    Create main.dart

---
+++ form_examle/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:music/pages/register_form_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Register',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: RegisterFormPage(),
+    );
+  }
+}
