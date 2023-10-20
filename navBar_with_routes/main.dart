aff63ef5e4091997054350f81e633e52a7c3f5b1
Ivan Gaydarzhi <112941873+vinny290@users.noreply.github.com>
2023-10-20 16:43:50 +0300

    Create main.dart

---
+++ navBar_with_routes/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:music/config/app_router.dart';
+import 'package:music/config/theme.dart';
+
+import 'screens/screens.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Zero To Unicorn',
+      theme: theme(),
+      onGenerateRoute: AppRouter.onGenerateRoute,
+      initialRoute: HomeScreen.routeName,
+    );
+  }
+}
